06f538ec5a1f38799b12d6ac074bc55dce51607b
amro.al-baali@mail.mcgill.ca <amro.al-baali@mail.mcgill.ca>
2021-02-23 13:02:02 -0500

    Implemented R2 node class

--- Nodes/BaseNode.m
+++ Nodes/BaseNode.m
@@ -12,6 +12,11 @@ classdef (Abstract) BaseNode
     %BASENODE Necessary functions and variables to implement a node class
     
     methods (Abstract = true)
+        % Public func: Value setter.
+        void = setValue(obj, value_in);
+        
+        % Public func: value getter
+        value_out = getValue(obj);
     end
     
     methods (Abstract = true, Static = true)
@@ -19,27 +24,38 @@ classdef (Abstract) BaseNode
         % can be used to access the node type and degrees of freedom (properties
         % that are not specific to any implementation)
         
-        % Returns the node type
-        type();
+        % Incrementing the values of the nodes. It updates the internal value by
+        % doing value = oplus( value, xi). E.g., for a LI-SE2: oplus(X, xi) = X
+        % * se2alg.expMap( - xi). Note that this is a Static function and can be
+        % used outside the class.
+        value = oplus(value, xi)
         
-        % Returns the dimensions or degrees of freedom of this class
-        dim();
+        % Verify element is of appropriate type/size/shape
+        bool = isValidValue( value);
+        
+        % Verify that the increment is of a valid type/size/shape. This is
+        % applicable when using oplus operator. The increment could be an
+        % element of the Lie algebra for example.
+        bool = isValidIncrement( increment);
     end
     
-    properties (Abstract = true)
+    properties (Abstract = true, Constant = true)
         %   The m_* prefix indicates that it's a 'member' variable
         
-        % static const string: Node type (e.g., "NodeSE2"). This could also by
-        % 'Name'
-        m_type;
-        
-        % NodeType: This is where the variable is stored (e.g., X_k pose). This
-        % will depend on the type of variable stored.
-        m_value;
+        % static const string: Node type (e.g., "NodeSE2"). I'll use the
+        % convention of setting m_type to the class name
+        type;
         
         % static const int: Dimention or degrees of freedom (dof) of the variable
         % (e.g., for SE(2), it's 3)
-        m_dim;
+        dim;
+    end
+    
+    properties (Abstract = true, Constant = false, SetAccess = protected, ...
+            GetAccess = protected)
+        % NodeType: This is where the variable is stored (e.g., X_k pose). This
+        % will depend on the type of variable stored.
+        m_value;
     end
     
 end
---
+++ Nodes/NodeR2.m
@@ -0,0 +1,73 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   An implementation of the BaseNode class. 
+%
+%   This is a 2D linear node. This should be generalized into multidimensional
+%   linear class (perhaps add another Abstract class that inhertics from
+%   BaseNode).
+%   
+%   Amro Al Baali
+%   23-Feb-2021
+%   --------------------------------------------------------------------------
+%   Change log
+%   --------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+classdef NodeR2 < BaseNode
+    %NODER2 Implementation of BaseNode on elements of R^2 space.
+    
+    methods (Access = public)
+        % Constructor
+        function obj = NodeR2()
+        end
+        % Public func: Value setter.
+        function obj = setValue(obj, value_in)
+            % Set the internal value
+            obj.m_value = value_in;
+        end
+                
+        function value_out = getValue(obj)
+            % Return the internal value
+            value_out = obj.m_value;
+        end
+        
+    end
+    
+    methods (Static = true)
+        % Static methods can be accessed without instantiating an object. These
+        % can be used to access the node type and degrees of freedom (properties
+        % that are not specific to any implementation)
+        
+        
+        % Increment value
+        function value_out = oplus(value_in, xi)
+            % Linear case
+            value_out = value_in + xi;
+        end
+        
+        % Check validity of the element
+        function isvalid = isValidValue( value_in)
+            % Value should be a [2x1] column matrix
+            isvalid = all( size( value_in) == [2, 1]);
+        end
+        
+        % Check validity of the increment
+        function isvalid = isValidIncrement( increment)
+            % This is the same as valid element
+            isvalid = NodeR2.isValidValue( increment);
+        end
+    end
+    
+    properties (Constant = true)
+        %   The m_* prefix indicates that it's a 'member' variable
+        
+        % Type of this node should match that class name
+        type = "NodeR2";
+        
+        % Dimension or degrees of freedom of this node
+        dim = 2;
+    end   
+    
+    properties (Constant = false, SetAccess = protected, GetAccess = protected)
+        % This keeps track of the value
+        m_value;
+    end
+end
